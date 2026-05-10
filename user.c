850cc862c1ad0c1398d20300d4985ad2852faa82
Z-X-Lin <z17392675801@163.com>
2026-05-10 15:54:28 +0800

    尝试完善

--- user.c
+++ user.c
@@ -1,41 +1,109 @@
-//豆包版
 #include "user.h"
 
-void userRegister() {
+#define USERS_FILE "data/users.txt"
+
+// 检查学号是否存在
+static int IDexist(const char *ID) {
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].ID, ID) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// 加载用户数据
+void loadUsers(void) {
+    FILE *fp = fopen(USERS_FILE, "r");
+    if (!fp) {
+        printf("用户数据文件不存在，将创建新文件\n");
+        return;
+    }
+    
+    userCount = 0;
+    while (fscanf(fp, "%s %s %s", users[userCount].ID, 
+                  users[userCount].name, users[userCount].password) == 3) {
+        userCount++;
+        if (userCount >= MAX_USERS) break;
+    }
+    fclose(fp);
+    printf("已加载 %d 位用户\n", userCount);
+}
+
+// 保存用户数据
+void saveUsers(void) {
+    FILE *fp = fopen(USERS_FILE, "w");
+    if (!fp) {
+        printf("无法保存用户数据！\n");
+        return;
+    }
+    
+    for (int i = 0; i < userCount; i++) {
+        fprintf(fp, "%s %s %s\n", users[i].ID, users[i].name, users[i].password);
+    }
+    fclose(fp);
+}
+
+// 用户注册
+void userRegister(void) {
     clear();
     printf("===== 用户注册 =====\n");
-    User u;
-    u.id = userCnt + 1;
-
-    printf("用户名：");
-    scanf("%s", u.username);
-    printf("密码：");
-    scanf("%s", u.pwd);
-
-    users[userCnt++] = u;
+    
+    user newUser;
+    
+    while (1) {
+        printf("请输入学号(12位): ");
+        scanf("%12s", newUser.ID);  // 限制最多读取12个字符
+        
+        if (strlen(newUser.ID) != 12) {
+            printf("学号必须为12位！\n");
+            continue;
+        }
+        
+        if (IDexist(newUser.ID)) {
+            printf("该学号已被注册！\n");
+            continue;
+        }
+        break;
+    }
+    
+    printf("请输入用户名: ");
+    scanf("%s", newUser.name);
+    
+    printf("请输入密码: ");
+    scanf("%s", newUser.password);
+    
+    users[userCount++] = newUser;
+    saveUsers();
+    
     printf("注册成功！\n");
     pause();
 }
 
-int userLogin() {
+// 用户登录
+int userLogin(void) {
     clear();
     printf("===== 用户登录 =====\n");
-    char name[20], pwd[20];
-
-    printf("用户名：");
-    scanf("%s", name);
-    printf("密码：");
-    scanf("%s", pwd);
-
-    for (int i = 0; i < userCnt; i++) {
-        if (strcmp(users[i].username, name) == 0 &&
-            strcmp(users[i].pwd, pwd) == 0) {
-            printf("登录成功！\n");
-            return users[i].id;
+    
+    char inputID[max_ID];
+    char inputPassword[max_password];
+    
+    printf("请输入学号: ");
+    scanf("%s", inputID);
+    printf("请输入密码: ");
+    scanf("%s", inputPassword);
+    
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].ID, inputID) == 0 && 
+            strcmp(users[i].password, inputPassword) == 0) {
+            memcpy(&currentUser, &users[i], sizeof(user));
+            printf("登录成功！欢迎 %s\n", currentUser.name);
+            pause();
+            return 1;
         }
     }
-
-    printf("账号或密码错误！\n");
+    
+    printf("学号或密码错误！\n");
     pause();
-    return -1;
+    return 0;
 }
